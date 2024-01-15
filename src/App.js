ef9998c62783da9914294a0af5cc3e25ad6ea7b8
Liliane-Top <hallo@lilianetop.nl>
2024-01-15 20:33:17 +0100

    added state and update state to buttons

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,21 +7,40 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  // create a state function with default value which you assign to 
+  // use => is a react hook 
+  // can only at top level not in an if statement or sub function
+  //you can't set the state manually you have to use the methos setStep function which is tied to state
+  const [step,  setStep] = useState(1);
+
+function handlePrevious() {
+  if(step > 1)
+  setStep(step -1);
+}
+
+function handleNext() {
+  if(step < 3)
+  setStep(step + 1);
+}
+
 
   return (
   <div className="steps">
     <div className="numbers">
-      <div className={`${step >= 1? "active" : ""}`}>1</div>
-      <div className={`${step >= 2? "active" : ""}`}>2</div>
-      <div className={`${step >= 3? "active" : ""}`}>3</div>
+      <div className={step >= 1? "active" : ""}>1</div>
+      <div className={step >= 2? "active" : ""}>2</div>
+      <div className={step >= 3? "active" : ""}>3</div>
     </div>
     <p className="message">
       Step {step}: {messages[step-1]} 
     </p>
     <div className="buttons">
-      <button style={{backgroundColor: "#7950f2", color: "#fff"}}>Previous</button>
-      <button style={{backgroundColor: "#7950f2", color: "#fff"}}>Next</button>
+      <button style={{backgroundColor: "#7950f2", color: "#fff"}} 
+      onClick={handlePrevious}
+      >Previous</button>
+      <button style={{backgroundColor: "#7950f2", color: "#fff"}} 
+      onClick={handleNext}
+      >Next</button>
     </div>
   </div>
   );
