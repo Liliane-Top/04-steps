ce2c886717544cc9928502d347a26fdd77f5a072
Liliane-Top <hallo@lilianetop.nl>
2024-01-16 13:55:54 +0100

    adding state for showing component or not

--- src/App.js
+++ src/App.js
@@ -12,36 +12,44 @@ export default function App() {
   // can only at top level not in an if statement or sub function
   //you can't set the state manually you have to use the methos setStep function which is tied to state
   const [step,  setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true)
 
 function handlePrevious() {
   if(step > 1)
-  setStep(step -1);
+  setStep((current) => current - 1);
 }
 
 function handleNext() {
-  if(step < 3)
-  setStep(step + 1);
+  if(step < 3) {
+    setStep((current) => current + 1);
+  }
+ 
 }
 
 
   return (
-  <div className="steps">
-    <div className="numbers">
-      <div className={step >= 1? "active" : ""}>1</div>
-      <div className={step >= 2? "active" : ""}>2</div>
-      <div className={step >= 3? "active" : ""}>3</div>
-    </div>
-    <p className="message">
-      Step {step}: {messages[step-1]} 
-    </p>
-    <div className="buttons">
-      <button style={{backgroundColor: "#7950f2", color: "#fff"}} 
-      onClick={handlePrevious}
-      >Previous</button>
-      <button style={{backgroundColor: "#7950f2", color: "#fff"}} 
-      onClick={handleNext}
-      >Next</button>
-    </div>
-  </div>
+<>
+  <button className="close" onClick={() => setIsOpen((open) =>  !open)}>&times;</button>
+    { isOpen && (
+      <div className="steps">
+        <div className="numbers">
+          <div className={step >= 1? "active" : ""}>1</div>
+          <div className={step >= 2? "active" : ""}>2</div>
+          <div className={step >= 3? "active" : ""}>3</div>
+        </div>
+        <p className="message">
+          Step {step}: {messages[step-1]} 
+        </p>
+        <div className="buttons">
+          <button style={{backgroundColor: "#7950f2", color: "#fff"}} 
+          onClick={handlePrevious}
+          >Previous</button>
+          <button style={{backgroundColor: "#7950f2", color: "#fff"}} 
+          onClick={handleNext}
+          >Next</button>
+        </div>
+      </div>
+    )}
+  </>
   );
 }
